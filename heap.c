f1a0d489d525315b5cca07ad503e35805ffe9fba
Replit user <>
2023-09-28 19:36:47 +0000

    new try

--- heap.c
+++ heap.c
@@ -48,7 +48,7 @@ void heap_push(Heap* pq, void* data, int priority){
     pq->heapArray[i]=nuevonodo;
     pq->size++;
     while(i!=0 && pq->heapArray[(i - 1) / 2].priority > pq->heapArray[i].priority){
-      swap(&(pq->heapArray[(i - 1) / 2]), &(pq->heapArray[i]));
+      swap(&(pq->heapArray[i]),&(pq->heapArray[(i - 1) / 2]));
       i=((i - 1) / 2);
     }
     
